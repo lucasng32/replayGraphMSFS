1e236c21a8c0dbd243d1bfdd77b75794d5e47b8c
lucasng32 <lucasng3.2@gmail.com>
2024-08-26 11:33:22 +0800

    Added WebSocketServer

---
+++ src/Plugins/Module/Server/src/WebSocketServer.h
@@ -0,0 +1,35 @@
+#ifndef WebSocketServer_H
+#define WebSocketServer_H
+
+#include <QtCore/QObject>
+#include <QtCore/QList>
+#include <QtCore/QByteArray>
+
+QT_FORWARD_DECLARE_CLASS(QWebSocketServer)
+QT_FORWARD_DECLARE_CLASS(QWebSocket)
+
+class WebSocketServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit WebSocketServer(quint16 port, bool debug = false, QObject *parent = nullptr);
+    ~WebSocketServer();
+
+    void sendData(const QJsonObject &data);
+
+Q_SIGNALS:
+    void closed();
+
+private Q_SLOTS:
+    void onNewConnection();
+    void processTextMessage(QString message);
+    void processBinaryMessage(QByteArray message);
+    void socketDisconnected();
+
+private:
+    QWebSocketServer *m_pWebSocketServer;
+    QList<QWebSocket *> m_clients;
+    bool m_debug;
+};
+
+#endif //WebSocketServer_H
